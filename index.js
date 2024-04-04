bc73ba3b27e1919d5e7e13b874fefb138fb6871a
Abdulhakim Kullow <abdulhakim.kullow@student.moringaschool.com>
2024-04-04 08:37:15 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution here
+function findMatching(driversArray, string){
+    return driversArray.filter(element => element.toLowerCase() === string.toLowerCase())
+}
+
+function fuzzyMatch(driversArray, string) {
+    return driversArray.filter(element => element.charAt(0) === string.charAt(0));
+}
+function matchName(driverObject, string){
+        return driverObject.filter(element => element.name === string)
+}
